3a26278a2b03edfb6106c0d61bd5d38ad1b237a9
Prasanth Nair <prasanthhn@gmail.com>
2011-10-08 12:18:25 +0530

    long_description from README.rst; add README.rst to MANIFEST.
    README.rst must be specified in MANIFEST, so that README.rst gets
    copied into the source distribution. Loading long_description at
    install time will fail if README.rst is not present in the
    source distribution.
    
    Added .gitignore.

--- setup.py
+++ setup.py
@@ -6,18 +6,21 @@ import jdcal
 
 version = jdcal.__version__
 
+long_description = open("README.rst").read()
+
 setup(
     name="jdcal",
     version=version,
     description="Julian dates from proleptic Gregorian and Julian calendars.",
-    long_description=open("README.rst").read(),
+    long_description=long_description,
     license='BSD',
     author="Prasanth Nair",
     author_email="prasanthhn@gmail.com",
-    url='https://github.com/phn/jdcal',
+    url='http://github.com/phn/jdcal',
     classifiers=[
         'Development Status :: 6 - Mature',
         'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: BSD License',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Programming Language :: Python',
